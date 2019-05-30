48f5dbd08d91b415ec0d8ba67b85a5acd3b1f098
Micah <mcbraun@uw.edu>
2019-05-30 15:19:40 -0700

    all files

---
+++ myblog/tests.py
@@ -0,0 +1,19 @@
+from myblog.models import Post
+
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+class PostTestCase(TestCase):
+    fixtures = ['myblog_test_fixture.json', ]
+
+    def test_string_representation(self):
+        expected = "This is a title"
+        p1 = Post(title=expected)
+        actual = str(p1)
+        self.assertEqual(expected, actual)
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+        from myblog.models import Post  # add this import at the top
+
+    
